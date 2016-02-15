61384a27fb16a55f4ec74a13a7a22e3fd55627bf
Matt Dean <mjdean1994@gmail.com>
2016-02-15 14:56:39 -0500

    Added Resources and Updated About Us page

---
+++ Athenaeum/Controllers/ResourcesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Athenaeum.Controllers
+{
+    public class ResourcesController : Controller
+    {
+        // GET: Resources
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult EditorGuide()
+        {
+            return View();
+        }
+
+        public ActionResult Timeline()
+        {
+            return View();
+        }
+    }
+}
